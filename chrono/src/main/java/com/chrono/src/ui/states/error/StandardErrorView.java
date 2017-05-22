aa55f5ce368d1e27757e1e5553ff71759c666c10
Filip Kowalski <filipp.kowalski@gmail.com>
2017-05-22 18:04:33 +0200

    Improved StandardErrorView

--- chrono/src/main/java/com/chrono/src/ui/states/error/StandardErrorView.java
+++ chrono/src/main/java/com/chrono/src/ui/states/error/StandardErrorView.java
@@ -14,21 +14,25 @@ import com.chrono.R;
  * Created by Filip Kowalski on 22.05.17.
  */
 
-public class StadardErrorView extends ErrorView {
+public class StandardErrorView extends ErrorView {
 
 	private TextView titleTextView;
 	private TextView subtitleTextView;
+	private Button button;
 
 	private String title;
 	private String subtitle;
+	private String buttonText;
 
 	private OnClickListener buttonClickListener;
 
-	public StadardErrorView(Context context, OnClickListener buttonClickListener, String title, String subtitle) {
+	public StandardErrorView(Context context, OnClickListener buttonClickListener, String title, String subtitle,
+	                         String buttonText) {
 		super(context);
 		this.buttonClickListener = buttonClickListener;
 		this.title = title;
 		this.subtitle = subtitle;
+		this.buttonText = buttonText;
 
 		init(context);
 	}
@@ -45,7 +49,7 @@ public class StadardErrorView extends ErrorView {
 		titleTextView = (TextView) view.findViewById(R.id.text_error_title);
 		subtitleTextView = (TextView) view.findViewById(R.id.text_error_subtitle);
 
-		Button button = (Button) view.findViewById(R.id.button_error);
+		button = (Button) view.findViewById(R.id.button_error);
 		button.setOnClickListener(buttonClickListener);
 
 		initValues();
@@ -54,6 +58,7 @@ public class StadardErrorView extends ErrorView {
 	private void initValues() {
 		titleTextView.setText(title);
 		subtitleTextView.setText(subtitle);
+		button.setText(buttonText);
 	}
 
 	@Override
